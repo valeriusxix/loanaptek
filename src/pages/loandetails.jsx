509f80de0172488132ad87d517bbb63bf4c264e1
Valerius <valeriusdecrypto@gmail.com>
2026-05-08 07:37:43 +0100

    updated loan details css in the .jsx version

--- src/pages/loandetails.jsx
+++ src/pages/loandetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import "./loanDetails.css";
+import "./loandetails.css";
 
 function LoanDetails() {
   const { id } = useParams();
